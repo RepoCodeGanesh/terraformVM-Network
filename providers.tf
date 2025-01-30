6ce91b7fbfb4a3c94afa4c08f9a32c76d17d774b
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2025-01-30 19:54:07 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -18,9 +18,9 @@ provider "azurerm" {
     }
   }
 
-  subscription_id = "e94bdd49-97a8-440d-bf40-10569cc9409e"
-  tenant_id       = "9a998602-7371-4737-98c5-cd71dd4a9e9b"
-  client_id       = "1ce6cbf4-1110-4ec8-b98d-80088bde1942"
+  subscription_id = "f4ffefe1-d689-4059-969c-ccc73e2a11d4"
+  tenant_id       = "4cef0d84-84d6-4ed0-8abe-773b015bcf99"
+  client_id       = "0dfa47eb-cb5f-4a19-8edc-192901b73c82"
   client_secret   = var.client_secret
 }
 
