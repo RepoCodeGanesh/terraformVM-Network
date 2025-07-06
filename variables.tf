ff1e448992eab5d821e265d7145908435ce975a7
Ganesan kasi <161068086+RepoCodeGanesh@users.noreply.github.com>
2025-07-06 22:57:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "resource_group_location" {
+/* variable "resource_group_location" {
   type        = string
   default     = "eastus"
-  description = "Location of the resource group."
-}
+ description = "Location of the resource group."
+} */
 
 variable "resource_group_name" {
   type        = string
