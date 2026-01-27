25b90e79d0497655af45ae9cb4603fadd7ca5993
Ganesan Kasi <RichTextForGanesh@outlook.com>
2026-01-27 23:10:53 +0530

    update

--- providers.tf
+++ providers.tf
@@ -12,5 +12,5 @@ provider "azurerm" {
   subscription_id = "f4ffefe1-d689-4059-969c-ccc73e2a11d4"
   tenant_id       = "4cef0d84-84d6-4ed0-8abe-773b015bcf99"
   client_id       = "0dfa47eb-cb5f-4a19-8edc-192901b73c82"
-  client_secret   = coalesce(file("${path.module}/client_secret.txt"), var.client_secret)
+  client_secret   = (var.client_secret)
 }
